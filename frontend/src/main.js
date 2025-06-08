e44603313675e62b2a90267bcb15cff84cf80f5a
Segond Nicolas <nicolassegond0@gmail.com>
2025-06-09 01:23:07 +0200

    Redesign the frontend files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '../src/router/router'
 
-createApp(App).mount('#app')
+import './assets/index.css' // ou ton CSS Tailwind
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
